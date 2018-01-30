9af8ddcdc3c1c29db1b9ad9cd8b1838c73526b6f
JHarrell1211 <julian1211@gmail.com>
2018-01-30 14:19:43 -0500

    first commit

---
+++ FizzBuzzExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i % 15) == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if ((i % 5) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if ((i % 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
